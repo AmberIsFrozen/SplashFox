b5ecd2e492190de9147445a5aaf634e5692180e4
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-03-27 18:19:28 +0800

    Finish Animated Loading Screen Compat
    Turns out we don't need to follow the method signature if we don't require any of the args

--- src/main/java/com/lx862/splashfox/mixin/compat/AnimatedLoadingScreenCompat.java
+++ src/main/java/com/lx862/splashfox/mixin/compat/AnimatedLoadingScreenCompat.java
@@ -1,7 +1,8 @@
-package com.lx862.splashfox.mixin;
+package com.lx862.splashfox.mixin.compat;
 
 import com.bawnorton.mixinsquared.TargetHandler;
-import net.minecraft.client.gui.DrawContext;
+import com.lx862.splashfox.SplashFox;
+import com.lx862.splashfox.data.ImagePosition;
 import net.minecraft.client.gui.screen.SplashOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,9 +15,10 @@ public class AnimatedLoadingScreenCompat {
             mixin = "com.cyao.animatedLogo.mixin.SplashOverlayMixin",
             name = "onAfterRenderLogo"
     )
-    @Inject(method = "@MixinSquared:Handler", at = @At("HEAD"))
-    private void b(DrawContext drawContext, int mouseX, int mouseY, float delta, CallbackInfo originalCi, int scaledWidth, int scaledHeight, long now, float fadeOutProgress, float fadeInProgress, float alpha, int x, int y, double height, int halfHeight, double width, int halfWidth, CallbackInfo ci) {
-        // We should conditionally cancel here
-        ci.cancel();
+    @Inject(method = "@MixinSquared:Handler", at = @At("HEAD"), cancellable = true)
+    private void cancelAnimatedLoadingScreen(CallbackInfo ci) {
+        if(SplashFox.config.position == ImagePosition.REPLACE_MOJANG) {
+            ci.cancel();
+        }
     }
 }
